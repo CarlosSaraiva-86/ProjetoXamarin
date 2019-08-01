5612d41451ec725e560e2880343f2e04b7b271ca
Antonio Gomes <antonio@evolucaoinfo.com.br>
2019-08-01 08:55:16 -0300

    Ajustes FB

--- BragantinaTelerikDemo/Android/FBLoginPageRenderer.cs
+++ BragantinaTelerikDemo/Android/FBLoginPageRenderer.cs
@@ -22,12 +22,13 @@ namespace BragantinaTelerikDemo.Android
 {
     public class FBLoginPageRenderer : PageRenderer
     {
-        public FBLoginPageRenderer(Context context)
+        public FBLoginPageRenderer(Context context) : base(context)
         {
             var activity = this.Context as Activity;
 
             var auth = new OAuth2Authenticator(
-                clientId: "2334801723209928",
+                clientId: "903786106680680",
+                //clientId: "2334801723209928",
                 scope: "",
                 authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
                 redirectUrl: new Uri("https://www.facebook.com/connect/login_success.html"));
@@ -39,13 +40,13 @@ namespace BragantinaTelerikDemo.Android
                     var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
                     var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
 
-                    var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me?fields=id,name,picture&access_token=ACCESS_TOKEN"), null, eventArgs.Account);
+                    var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me?fields=id,name,picture,email&access_token=ACCESS_TOKEN"), null, eventArgs.Account);
                     var response = await request.GetResponseAsync();
                     var obj = JObject.Parse(response.GetResponseText());
 
                     var id = obj["id"].ToString().Replace("\"", "");
                     var name = obj["name"].ToString().Replace("\"", "");
-                    //var email = obj["email"].ToString().Replace("\"", "");
+                    var email = obj["email"].ToString().Replace("\"", "");
                     //var tel = obj["mobile_phone"].ToString().Replace("\"", "");
                     var picture = obj["picture"]["data"]["url"];
 
